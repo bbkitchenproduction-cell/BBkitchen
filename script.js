3bc34b15f57858fcc7ff46aee7b97d2b140a55e1
BBkitchen <bbkitchenproduction@gmail.com>
2025-09-30 05:37:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,35 @@ document.querySelector('.prev')?.addEventListener('click', () => showSlide(curre
 setInterval(() => {
   showSlide(currentIndex + 1);
 }, 4000);
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(${100 * (i - index)}%)`;
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  showSlide(currentSlide);
+}
+
+// Events للأزرار
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
+
+// Auto play كل 4 ثواني
+setInterval(nextSlide, 4000);
+
+// تشغيل أول Slide
+showSlide(currentSlide);
